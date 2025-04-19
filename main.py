1bc406d645b12677fcc8de93c586dd9a9d3aabdd
mishimaaa <trendeleev@outlook.com>
2025-04-19 18:10:15 +0900

    commit to test branch

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from yuhangli!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ test.py
@@ -0,0 +1,5 @@
+import sys
+print("Python is running!")
+print("Executable:", sys.executable)
+print("Version:", sys.version)
+print('what is wrong with you?')
